bc091016e18e87c892ccdcb42ba881c89edb7c20
Marcelo <marceloriosrodriguez@gmail.com>
2019-06-30 16:44:17 -0500

    +Ch Marcelo
    Componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ReservacionComponent } from './components/reservacion/reservacion.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'productos', component: ProductosComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'reservacion', component: ReservacionComponent},
+  {path: 'galeria', component: GaleriaComponent},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ReservacionComponent } from './components/reservacion/reservacion.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page404Component,
+    NosotrosComponent,
+    ReservacionComponent,
+    ProductosComponent,
+    GaleriaComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
